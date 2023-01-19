992c706367a1f5dcad34802b959aeea0362af312
sash9696 <67851308+sash9696@users.noreply.github.com>
2023-01-19 19:46:42 +0530

    Add files via upload

--- react/pokemon-react/src/App.js
+++ react/pokemon-react/src/App.js
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import pokemon from './pokemon.json'
+import PropTypes from 'prop-types'
+
+
+const PokemonType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.shape({
+    english: PropTypes.string.isRequired,
+    japanese: PropTypes.string.isRequired,
+    chinese: PropTypes.string.isRequired,
+    french: PropTypes.string.isRequired,
+  }),
+  type: PropTypes.arrayOf(PropTypes.string.isRequired),
+  base: PropTypes.shape({
+    hp: PropTypes.number.isRequired,
+    attack: PropTypes.number.isRequired,
+    defense: PropTypes.number.isRequired,
+    special_attack: PropTypes.number.isRequired,
+    special_defense: PropTypes.number.isRequired,
+    speed: PropTypes.number.isRequired,
+  })
+
+
+})
 
 const PokemonRow = ({ pokemon }) => {
-  // console.log(props.pokemon)
   return (
 
     <tr>
       <td>{pokemon.name.english}</td>
       <td>{pokemon.type.join(", ")}</td>
+      <td><button>More Information</button></td>
     </tr>
 
 
   )
 }
 
+PokemonRow.propTypes = {
+  pokemon: PokemonType
+}
+
 
 
 
 function App() {
+
+  //two rules of hooks
+  //it should be on top level management
+  //it should be inside react function
+  const [filter, setFilter] = useState('')
+
+  console.log(filter)
+
+  const searchPokemon = (event) => {
+    setFilter(event.target.value)
+  }
   return (
     <div style={{
       margin: 'auto',
@@ -26,22 +64,45 @@ function App() {
       paddingTop: '1em'
     }} >
       <h1 className='title'>Pokemon Search</h1>
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: "80% 20%",
+          gridColumnGap: '1rem'
+        }}
+      >
+        <div>
+          <input
+            type='text'
+            value={filter}
+            onChange={searchPokemon}
 
-        <tbody>
+          />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
 
-          {pokemon.map((pokemon) => (
-            <PokemonRow key={pokemon.id} pokemon={pokemon} />
-          )
-          )}
-        </tbody>
-      </table>
+            <tbody>
+
+              {pokemon.filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase())).slice(0, 20).map((pokemon) => (
+                <PokemonRow key={pokemon.id} pokemon={pokemon} />
+              )
+              )}
+              {/* {pokemon.filter(({ name: { english } }) => english.includes(filter)).slice(0, 20).map((pokemon) => (
+                <PokemonRow key={pokemon.id} pokemon={pokemon} />
+              )
+              )} */}
+            </tbody>
+          </table>
+        </div>
+
+
+      </div>
+
     </div>
   )
 }
